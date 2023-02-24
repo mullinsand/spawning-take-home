39ff43a18ebf430d19521e2525008205dff26a8f
Andrew Mullins <mullinsand15@gmail.com>
2023-02-24 10:03:57 -0700

    moved map logic from app to product index component

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,10 @@
 import './App.css';
-import Data from "../products.json"
+import ProductIndex from '../ProductIndex/ProductIndex';
 
 function App() {
   return (
     <div>
-      {
-        Data.map((product) => {
-          return (
-          <div className="product">
-            <p>Product Name: {product.name}</p>
-            <p>Description: {product.description}</p>
-            <p>Category: {product.category}</p>
-            <p>Price: {product.price}</p>
-            <p>Quantity: {product.quantity}</p>
-            <img src={product.image} alt={`Image of ${product.name}`}/>
-            <p>Weight: {product.weight}</p>
-            <p>Model: {product.model}</p>
-          </div>
-          )
-        })
-      }
+      <ProductIndex />
     </div>
   )
 }
---
+++ src/ProductIndex/ProductIndex.js
@@ -0,0 +1,27 @@
+import './ProductIndex.css';
+import Data from "../products.json"
+
+function ProductIndex() {
+  return (
+    <div>
+      {
+        Data.map((product) => {
+          return (
+          <div className="product">
+            <p>Product Name: {product.name}</p>
+            <p>Description: {product.description}</p>
+            <p>Category: {product.category}</p>
+            <p>Price: {product.price}</p>
+            <p>Quantity: {product.quantity}</p>
+            <img src={product.image} alt={`Image of ${product.name}`}/>
+            <p>Weight: {product.weight}</p>
+            <p>Model: {product.model}</p>
+          </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default ProductIndex;
