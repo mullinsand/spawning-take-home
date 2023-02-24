babf45c6d5359d83bfff7d965dbc48c64bfc5329
Andrew Mullins <mullinsand15@gmail.com>
2023-02-24 09:54:05 -0700

    product index shows all attributes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ function App() {
         Data.map((product) => {
           return (
           <div className="product">
-            <p>{product.name}</p>
+            <p>Product Name: {product.name}</p>
+            <p>Description: {product.description}</p>
+            <p>Category: {product.category}</p>
+            <p>Price: {product.price}</p>
+            <p>Quantity: {product.quantity}</p>
+            <img src={product.image} alt={`Image of ${product.name}`}/>
+            <p>Weight: {product.weight}</p>
+            <p>Model: {product.model}</p>
           </div>
           )
         })
